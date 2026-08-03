04a5f047ac092347c0ad77a755117c1eba0d9152
zahidriskyrabbani <zahidriskyrabbani@gmail.com>
2026-08-03 17:25:57 +0700

    button

--- script.js
+++ script.js
@@ -115,3 +115,39 @@ scrollTopBtn.addEventListener("click",()=>{
     });
 
 });
+
+// =========================
+// MOBILE MENU
+// =========================
+
+const menuToggle = document.getElementById("menuToggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+
+        menuToggle.textContent = "✕";
+
+    }else{
+
+        menuToggle.textContent = "☰";
+
+    }
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        navLinks.classList.remove("active");
+
+        menuToggle.textContent="☰";
+
+    });
+
+});
