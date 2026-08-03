c3e1dc9050084c643e55521a745a387bd924ef28
zahidriskyrabbani <zahidriskyrabbani@gmail.com>
2026-08-03 16:12:45 +0700

    scroll to top button

--- script.js
+++ script.js
@@ -83,3 +83,35 @@ themeToggle.addEventListener("click", () => {
     }
 
 });
+
+// =========================
+// SCROLL TO TOP
+// =========================
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY > 300){
+
+        scrollTopBtn.classList.add("show");
+
+    }else{
+
+        scrollTopBtn.classList.remove("show");
+
+    }
+
+});
+
+scrollTopBtn.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
